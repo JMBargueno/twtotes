c65d28d5dc45d9534bb49bdf986f633e613d8c0c
jmbargueno <jmbarguenolopez@gmail.com>
2020-11-29 01:38:52 +0100

    Soporte a multitarget

--- src/index.ts
+++ src/index.ts
@@ -4,20 +4,15 @@ require("dotenv").config();
 
 //https://kaffeine.herokuapp.com/
 
-async function assertNewTweet() {
+async function assertNewTweet(target: string) {
   let twitterClient = new TwitterClient({
     apiKey: process.env.API_KEY!,
     apiSecret: process.env.API_SECRET!,
     accessToken: process.env.ACCESS_TOKEN!,
     accessTokenSecret: process.env.ACCESS_TOKEN_SECRET!,
   });
-  let twitterBot = new TwitterBot(
-    process.env.TARGET!,
-    twitterClient,
-    process.env.FRIEND!
-  );
+  let twitterBot = new TwitterBot(target, twitterClient, process.env.FRIEND!);
 
- twitterBot.getRandomFriend()
   await twitterBot.getUser().then((result) => {
     console.log(
       `\nInstante: ${new Date().toLocaleString()}\nTweet: ${
@@ -32,18 +27,25 @@ async function assertNewTweet() {
   //
 }
 
+function getTargetArray() {
+  return process.env.TARGETS!.split(",");
+}
+
+async function doStuff() {
+  const targets = getTargetArray();
+
+  for (let target of targets) {
+    console.log("\n##########################################");
+    console.log(`\nNuevo try a ${target}`);
+    await assertNewTweet(target);
+    console.log("\n##########################################");
+  }
+}
+
 console.log(
   `TWTOTES iniciado con un intervalo de ${Number(
     process.env.INTERVAL_S!
   )} segundos`
 );
-
-
-async function doStuff() {
-  console.log("\n##########################################");
-  console.log("\nNuevo try");
-  await assertNewTweet();
-  console.log("\n##########################################");
-}
 
 setInterval(doStuff, Number(process.env.INTERVAL_S!) * 1000);
